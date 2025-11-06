11c997658960a4b36006916e7d7193e06aad42e7
Adenosine030 <ademolaa.adenigba@gmail.com>
2025-11-06 13:55:09 -0800

    Day 1/30: Hello World complete! 🚀

---
+++ day01_hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+name="CloudDemigod"
+echo "Hello, Devops world"
+echo "I am $name and I am starting my 30-day journey!"
+date
+
---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+NAME= CloudDemigod
+echo "Hello, Devops world"
+echo "I am $NAME and I am starting my 30-day journey!"
+date
+
