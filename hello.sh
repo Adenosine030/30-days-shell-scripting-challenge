a76728b582b7ff2c6d1c4ba6c68d3e6d8101ef68
Adenosine030 <ademolaa.adenigba@gmail.com>
2025-11-07 13:45:18 -0800

    Resolve merge conflicts

--- hello.sh
+++ hello.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-NAME= CloudDemigod
-echo "Hello, Devops world"
-echo "I am $NAME and I am starting my 30-day journey!"
+echo "What is your name? "
+read NAME
+echo "Hello, $NAME! what is your age?"
+read AGE
+echo "Hey $NAME, you are $AGE years old."
 date
 
